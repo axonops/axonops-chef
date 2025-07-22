bc3dd83f50add0e2930218fa021eec43a10ee20a
Sergio Rua <sergio@axonops.com>
2025-07-22 10:17:01 +0100

    Alerts

--- recipes/alert_rules.rb
+++ recipes/alert_rules.rb
@@ -6,7 +6,7 @@
 #
 
 # Example 1: CPU usage alert
-axonops_alert_rule 'high_cpu_usage' do
+axonops_alert_rule 'Check for High CPU' do
   org ENV['AXONOPS_ORG'] || node['axonops']['api']['org']
   cluster ENV['AXONOPS_CLUSTER'] || node['axonops']['api']['cluster']
   username ENV['AXONOPS_USERNAME'] || node['axonops']['api']['username'] || ''
@@ -14,13 +14,33 @@ axonops_alert_rule 'high_cpu_usage' do
   base_url ENV['AXONOPS_URL'] || node['axonops']['api']['base_url'] || ''
   auth_token ENV['AXONOPS_TOKEN'] || node['axonops']['api']['auth_token'] || ''
   dashboard 'System'
-  chart 'CPU Usage'
-  metric 'cpu_usage_percent'
+  chart 'CPU usage per host'
+  metric 'host_CPU_Percent_Merge'
   operator '>'
   warning_value 80
   critical_value 90
   duration '5m'
   description 'Alert when CPU usage is too high'
+  routing ['slack-alerts', 'pagerduty']  # Integration names
+  action :create
+end
+
+# Example 2: Memory usage alert
+axonops_alert_rule 'high_memory_usage' do
+  org ENV['AXONOPS_ORG'] || node['axonops']['api']['org']
+  cluster ENV['AXONOPS_CLUSTER'] || node['axonops']['api']['cluster']
+  username ENV['AXONOPS_USERNAME'] || node['axonops']['api']['username'] || ''
+  password ENV['AXONOPS_PASSWORD'] || node['axonops']['api']['password'] || ''
+  base_url ENV['AXONOPS_URL'] || node['axonops']['api']['base_url'] || ''
+  auth_token ENV['AXONOPS_TOKEN'] || node['axonops']['api']['auth_token'] || ''
+  dashboard 'System'
+  chart 'Used memory'
+  metric 'host_Memory_Used'
+  operator '>'
+  warning_value 80
+  critical_value 90
+  duration '5m'
+  description 'Alert when memory usage is too high'
   routing ['slack-alerts', 'pagerduty']  # Integration names
   action :create
 end
